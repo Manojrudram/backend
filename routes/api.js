ddb38279ba34200be141987e48b81d13024dc63c
MANOJMKR\Manoj kumar <manojkumar.rudram@gmail.com>
2017-10-10 23:46:35 +0530

    FCM added

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,56 @@ var mongoose = require('mongoose');
 var config = require('../config/database');
 var Admin = require('../app/models/admin');
 
+var FCM = require('fcm-push');
 
 var app = express();
 var router = express.Router();
 
+//Config FCM
+var serverKey = 'AAAAfbJMAYw:APA91bHGAT03AbmeYZNUITHHBbV0jdveRjkxBgWVGwtnT4mQ15Gf8ojPFQysYvWwKzC46XZKJJbqcavPhC12wB_Wj0opDagNW1XL4nSBE54qckzx2zIj0LtAWqnb4X7OV9LhIJ-6rXa7';
+var fcm = new FCM(serverKey);
+
+
 
 mongoose.connect(config.database);
 app.set('superSecret',config.secret);
+
+
+
+
+/*FCM Push Notification */
+router.post('/test', function(req, res, next) {
+
+	    var message = {
+        to: 'dwvZUxVcmdU:APA91bE2SNJSnZjGGx_SG2lkPo3r1stLsCINDdINP3BA1nbkbtis-GUTWH3CtjGgr2VPDsiYqwrl61Yz2jeh_YRf7-t2yhWtanz4Y-WGk3oVdIsK2wtVQUqE4x1pv6M-dg8JdRLdr1iE', // required fill with device token or topics
+        //collapse_key: 'your_collapse_key', 
+        data: {
+        	   id: req.body.id,
+            page: req.body.type
+        },
+        notification: {
+            title: 'Kutit'
+        
+        }
+    };
+
+
+	  //promise style
+    fcm.send(message)
+        .then(function(response){
+            console.log("Successfully sent with response: ", response);
+            res.send({"message":"sucess"});
+
+        })
+        .catch(function(err){
+            console.log("Something has gone wrong!");
+            console.error(err);
+            res.send({"message":"failure"});
+        })
+
+	});
+
+
 
 router.post('/signup', function(req, res, next) {
 	 console.log(JSON.stringify(req.body))
